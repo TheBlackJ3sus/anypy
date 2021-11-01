bfa1f7cf1ef122b73f51ceb2b62f7ac51e36b4d7
Kas <khasimbradley@gmail.com>
2021-11-01 09:30:51 -0400

    Update any.py
    Still simple and ugly, but preparing for more control. I've also realized I may be doing something wrong with pywinauto as it takes away the users mouse for a brief second  to invoke clicks. Which isn't a big deal if you run this overnight or when you're not using the computer but annoying when you want this running in the background. Will try to find a way with pywinauto. Recent update makes initial progressbar detection slow, gotta see what changed or find a direct route.  Also..  I'm running this on it's trial version and I was able to crack the software to get around VM detection but I wont be be able to share instructions or the exported x64dgb patch. Just a note saying it can be done.

--- any.py
+++ any.py
@@ -1,6 +1,57 @@
 from pywinauto.application import Application
 from pywinauto.findwindows import ElementNotFoundError
 from pywinauto.findbestmatch import MatchError
+import time
+def SelectSeason(DownloadWindow,sn):
+    DownloadWindow.ComboBox.select(f'Season {sn}')
+
+def GetSeasons(DownloadWindow):
+    NumSeasons = DownloadWindow.ListBox.item_count()
+    SeasonList = []
+    count = 0
+    while count < NumSeasons:
+        count +=1
+        SeasonList.append(f'Season {count}')
+    return SeasonList
+    
+def EpiBts(DownloadWindow,ShowName,sn):
+    print(f'Grabbing episodes from {ShowName} Season {sn}...')
+    Episodes = DownloadWindow.descendants(class_name='DownloadableDetails')
+    DownloadBts = DownloadWindow.descendants(class_name='QPushButton')
+    EpisodeList = {"Title":[],"Button":[]}
+    count = 0
+    for episode,downloadbt in zip(Episodes, DownloadBts):
+        count +=1
+        print(episode.children_texts()[1])
+        EpisodeList["Title"].append(episode.children_texts()[1])
+        EpisodeList["Button"].append(f'DownloadWindow.DownloadButton{count}')
+    return EpisodeList  
+
+def DownloadEpis(EpisodeList):
+    for EpisodeTitle, EpisodeDownload in zip(EpisodeList["Title"], EpisodeList["Button"]):
+        DownloadDone = False
+        print(f'Downloading.. {EpisodeTitle}')
+        eval(EpisodeDownload).click()
+        DownloadConfig.DownloadButton.click()
+        if app.FileExists.exists(timeout=10):
+            app.FileExists.NoButton.click()
+            print(f'Skipping.. {EpisodeTitle} (Already Downloaded)')
+            DownloadWindow.wait('visible', timeout=1000)
+            continue
+        Progressbar.NetflixProgress.wait('visible')
+        before = int(Progressbar.NetflixProgress.legacy_properties()['Value'])
+        print(f'Progress... {before / 100}%', end="")
+        while DownloadDone is False:
+            after = int(Progressbar.NetflixProgress.legacy_properties()['Value'])
+            if after == 10000:
+                print('\r Progress... (Download Complete)')
+                DownloadDone = True
+            else:
+                if after != before:
+                    print(f'\r Progress... {after / 100}%', end="")
+                    before = after
+        DownloadWindow.wait('visible', timeout=1000)
+    
 
 print('Connecting to AnyStream...')
 try:
@@ -14,53 +65,20 @@ except ElementNotFoundError:
     mainwindow.wait('ready')
 print('Connected to AnyStream')
 
+###Future Specs
+DownloadConfig = mainwindow.child_window(title="Download configuration", class_name="DownloadConfigurationNetflix", visible_only = False, found_index=0)
+Progressbar = mainwindow.child_window(class_name="MultiBar", visible_only = False)
 
 print('Waiting for user to find a show...')
 DownloadWindow = mainwindow.child_window(title='Downloadable item(s)', class_name='DownloadableViewDialog', visible_only = False)
 DownloadWindow.wait('enabled', timeout=1000)
 ShowName = ''.join(DownloadWindow.Static1.texts())
 
-
-print('Found a selection')
-print(f'Grabbing episodes from {ShowName}...')
-Episodes = DownloadWindow.descendants(class_name='DownloadableDetails')
-DownloadBts = DownloadWindow.descendants(class_name='QPushButton')
-EpisodeList = {"Title":[],"Button":[]}
+Seasons = GetSeasons(DownloadWindow)
 count = 0
-for episode,downloadbt in zip(Episodes, DownloadBts):
+for dlseason in Seasons:
     count +=1
-    print(episode.children_texts()[1])
-    EpisodeList["Title"].append(episode.children_texts()[1])
-    EpisodeList["Button"].append(f'DownloadWindow.DownloadButton{count}')
-
-
-###Future Specs
-DownloadConfig = mainwindow.child_window(title="Download configuration", class_name="DownloadConfigurationNetflix", visible_only = False, found_index=0)
-Progressbar = mainwindow.child_window(class_name="MultiBar", visible_only = False)
-
-
-print('Starting Downloads...')
-for EpisodeTitle, EpisodeDownload in zip(EpisodeList["Title"], EpisodeList["Button"]):
-    DownloadDone = False
-    print(f'Downloading.. {EpisodeTitle}')
-    eval(EpisodeDownload).click()
-    DownloadConfig.DownloadButton.click()
-    if app.FileExists.exists(timeout=10):
-        app.FileExists.NoButton.click()
-        DownloadWindow.wait('visible', timeout=1000)
-        continue
-    Progressbar.NetflixProgress.wait('visible')
-    mainwindow.minimize()
-    before = int(Progressbar.NetflixProgress.legacy_properties()['Value'])
-    print(f'Progress... {before / 100}%', end="")
-    while DownloadDone is False:
-        after = int(Progressbar.NetflixProgress.legacy_properties()['Value'])
-        if after == 10000:
-            print('\r Progress... (Download Complete)')
-            DownloadDone = True
-        else:
-            if after != before:
-                print(f'\r Progress... {after / 100}%', end="")
-                before = after
-    mainwindow.restore()
-    DownloadWindow.wait('visible', timeout=1000)
+    SelectSeason(DownloadWindow,count)
+    time.sleep(1)
+    EpiandBtList = EpiBts(DownloadWindow,ShowName,count)
+    DownloadEpis(EpiandBtList)
